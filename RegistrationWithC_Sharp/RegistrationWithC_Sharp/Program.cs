b81fcd60f97965e1f5c6df06d9785dc77749c71d
Хамелеон <maxali946@gmail.com>
2017-11-16 17:34:22 +0400

    100%

--- RegistrationWithC_Sharp/RegistrationWithC_Sharp/Program.cs
+++ RegistrationWithC_Sharp/RegistrationWithC_Sharp/Program.cs
@@ -14,64 +14,128 @@ namespace TestC_Sharp
         static int Age;
         static string Username;
         static string Password;
+        
         //---------------------------------------------------------------------
         static string GenerateUserName()
         {
+            string Buffer = Name;
+            if (Name.Length > 4)
+                Buffer = Name.Remove(4/*, Name.Length - 4*/);
+            Username = Buffer.ToUpper() + '_';
 
-            //Username = Name.ToUpper() +
-                return "";
+            Buffer = Surname.ToLower();
+            Username += Buffer.Remove(2/*, Surname.Length - 2*/) + '_' + Age;
 
+            return Username; 
         }
         //---------------------------------------------------------------------
-        static void Registration()
+        static string GeneratePassword()
+        {
+            Random NL = new Random();
+            Random LL = new Random();
+            bool @switch = false;
+
+            for (int i = 0; i < 8; i++)
+            {
+                //UL = new Random();
+                //LL = new Random();
+
+                if (@switch)
+                    Password += (char)NL.Next(48, 57);
+                else
+                    Password += (char)LL.Next(97, 122);
+
+                @switch = !@switch;
+            }
+
+            return Password;
+        }
+        //---------------------------------------------------------------------
+        static bool Registration(bool reg)
+        {
+            if (reg)
+            {
+                Console.Clear();
+                Console.WriteLine("Sign Up\nYou are aleady ready!");
+                return reg;
+            }
+            else
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.Write("Sign Up\nEnter your name:\t");
+                    Name = Console.ReadLine();
+                    Console.Write("Enter your surname:\t");
+                    Surname = Console.ReadLine();
+                    Console.Write("Enter your age:\t\t");
+                    int.TryParse(Console.ReadLine(), out Age);
+
+                    Console.WriteLine("------------------------------------------------------------------------------------\n");
+                    Console.Write("Your username is:\t");
+                    Console.WriteLine(GenerateUserName());
+                    Console.Write("Your password is:\t");
+                    Console.WriteLine(GeneratePassword());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+
+                return true;
+            }
+        }
+        //---------------------------------------------------------------------
+        static void LogIn()
         {
             Console.Clear();
-            Console.Write(@"\tSignUp:
-							Enter your name:\t");
-            Name = Console.ReadLine();
-            Console.Write("Enter your surname:\t");
-            Surname = Console.ReadLine();
-            Console.Write("Enter your age:\t");
-            Age = Console.Read();
-            Console.WriteLine(@"
-								Your UserName:\t");
+            Console.Write("Log In\nEnter username:\t");
+            string TempUsername = Console.ReadLine();
+            Console.Write("Enter password:\t");
+            string TempPassword = Console.ReadLine();
 
-
+            Console.WriteLine("------------------------------------------------------------------------------------\n");
+            if (TempPassword == Password && TempUsername == Username)
+                Console.WriteLine($"Welcome, {Name} {Surname}!");
+            else
+                Console.WriteLine("Wrong username or password!");
         }
         //---------------------------------------------------------------------
-        static void LogUp()
+        static void Pause()
         {
-
-        }
-        //---------------------------------------------------------------------
-        static string GeneratePassword(int length)
-        {
-            return "asd";
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
         }
         //---------------------------------------------------------------------
         static void Main(string[] args)
         {
             int select;
+            bool registered = false;
+
             while (true)
             {
-                Console.WriteLine(@"1. Sign Up
-									2. Sign In
-									3. Exit
-									>");
+                Console.Write("1. Sign Up\n2. Log In\n3. Exit\n>");
 
-                select = Console.Read();
+                select = Console.ReadKey(true).KeyChar;
 
                 switch (select)
                 {
-                    case 1:
-                        Registration();
-
+                    case 49: // 1
+                        registered = Registration(registered);
+                        Pause();
+                        Console.Clear();
                         break;
-                    default:
+                    case 50: // 2
+                        LogIn();
+                        Pause();
+                        Console.Clear();
+                        break;
+                    case 51: // 3
+                        Environment.Exit(0);
                         break;
                 }
             }
-            //---------------------------------------------------------------------
         }
     }
 }
